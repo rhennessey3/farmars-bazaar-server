0104b873ca1ea46f5612c593246da3e01eb8ea9c
rick <rhennessey3@gmail.com>
2020-11-14 13:49:20 -0500

    changed config.js to include the postgres URI

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/farmers-bazaar',
+    DATABASE_URL: process.env.DB_URL || 'postgres://jxsjdcwrehagbq:3c02a47f10871c3fe78046a1f6c01a8ad0c3a997152736e3de5ae84baaf9cd2e@ec2-34-237-247-76.compute-1.amazonaws.com:5432/d8k1p8acthhnt0',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     CLIENT_ORIGIN: '*'
 }
